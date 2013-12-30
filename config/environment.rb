534e27c2fec04d6700edd78eb1e1211ffd4e307f
nitin <nitinzd1985@hotmail.com>
2013-12-30 15:57:26 +0530

    modified mail settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 JoshIntranet::Application.initialize!
-config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'joshintranet.com',
+    domain:               'niwantintranet.com',
     user_name:            'niwant.techvision@gmail.com',
     password:             'namv1234',
     authentication:       'plain',
     enable_starttls_auto: true }
 
-Date::DATE_FORMATS.merge!(:default => "%d/%m/%Y")
+  Date::DATE_FORMATS.merge!(:default => "%d/%m/%Y")
