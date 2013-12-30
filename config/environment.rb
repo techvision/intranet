e79023918744becaba1f16d411d8b01164b8ab9e
nitin <nitinzd1985@hotmail.com>
2013-12-30 15:43:51 +0530

    Have confugered email addon for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 JoshIntranet::Application.initialize!
+config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'joshintranet.com',
+    user_name:            'niwant.techvision@gmail.com',
+    password:             'namv1234',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
 Date::DATE_FORMATS.merge!(:default => "%d/%m/%Y")
