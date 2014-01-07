de7c1a739c9c12c8417811fc37bb4f5b741e7983
sanghapal <sanghapalbhowate89@gmail.com>
2014-01-07 12:09:17 -0500

    integrate the accessible calendar in LMS. add the related javascript and css files.

---
+++ app/assets/javascripts/setup_apply_leave.js
@@ -0,0 +1,31 @@
+$A.bind(window, 'load', function(){
+
+	// Syntax : setCalendar( ID , TriggeringElement , TargetEditField , EnableComments , clickHandler , config )
+
+	// Basic calendar with comment editing disabled
+
+	$A.setCalendar('BasicCalendar', $A.getEl('imgCalendar_start_date'), $A.getEl('leave_starts_at'), false, function(ev, dc, targ)
+//	$A.setCalendar('BasicCalendar', $A.getEl('imgCalendar_end_date'), $A.getEl('leave_ends_at'), false, function(ev, dc, targ)
+{
+		// Return the desired date string
+		targ.value = dc.range.wDays[dc.range.current.wDay].lng + ' ' + dc.range[dc.range.current.month].name + ' '
+			+ dc.range.current.mDay + ', ' + dc.range.current.year;
+		// Then close the date picker
+		dc.close();
+	});
+
+	// Advanced calendar with comment editing enabled
+
+	$A.setCalendar('AdvancedCalendar', $A.getEl('imgCalendar_end_date'), $A.getEl('leave_ends_at'), false, function(ev, dc, targ){
+		targ.value = dc.range.wDays[dc.range.current.wDay].lng + ' ' + dc.range[dc.range.current.month].name + ' '
+			+ dc.range.current.mDay + ', ' + dc.range.current.year;
+		dc.close();
+	},
+					{
+					// Display the editor
+					editor:
+									{
+									show: true
+									}
+					});
+});
