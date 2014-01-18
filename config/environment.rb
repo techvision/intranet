9e34230d03a5d891d95b1ba02d19ff087059d52f
Nitin Dhaware <nitin@techvision.net.in>
2014-01-18 11:40:01 +0530

    Have changed mail settings for production.

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 JoshIntranet::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+   if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+			   end
 
 Date::DATE_FORMATS.merge!(:default => "%d/%m/%Y")
